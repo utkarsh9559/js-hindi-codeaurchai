185026ac9e1bcefb409b38597ce0fa5ad7a66856
utkarsh9559 <singhutkarsh9559@gmail.com>
2025-04-26 11:18:06 +0000

    conversionOperatrion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,24 @@
+let score = true
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+//"33"=> 33
+//"33abc" => nan(not a number)
+//true 1 ; false 0 
+let isLoggedIn = 0
+//1 => true 0=>false 
+
+ let booleanIsLoggedIn = Boolean(isLoggedIn);
+ console.log(booleanIsLoggedIn);
+
+  let someNUmber = 33 
+  let stringNumber = String(someNUmber)
+  console.log(stringNumber);
+  console.log(typeof stringNumber);
+  
+  

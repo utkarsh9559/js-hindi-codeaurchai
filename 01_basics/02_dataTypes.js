ddf7a98a071264ba37e045fd751da9d6ed4f30ac
utkarsh9559 <singhutkarsh9559@gmail.com>
2025-04-26 10:58:00 +0000

    dataTypes

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -3,4 +3,18 @@
 //alert(3+3) //we are using nodejs, not browser
 
 console.log(3+3); 
-console.log()
+console.log("utkarsh")
+ 
+let name = "utkarsh"
+let age = 18
+let isLoggedIn = false 
+let state = null
+//number => 2 to power 53
+//bigint
+//string 
+//boolean =>true/false
+//null =>standalone value 
+//undefined => 
+    // symbol  =>unique 
+//object
+console.log(typeof "utkarsh");

3326b1553ded12f62918cd14dbe62550691f34a3
utkarsh9559 <singhutkarsh9559@gmail.com>
2025-04-26 10:25:10 +0000

    container conf done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 144553
+let accountEmail = "utkarsh@google.com"//used to declare variables (most used)
+var accountpassword = "12345"// general we do not use it , because of scope problem
+accountCity = "jaipur"
+let accountstate;
+
+// accountId = 2 // not allowed when you are using const keyword
+ accountEmail = "hc@hc.com"
+ accountpassword = "224221"
+ accountCity = " delhi"
+console.log(accountId);
+console.table([accountId , accountEmail , accountpassword , accountCity , accountstate])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,6 @@
+"use strict"; //treat all Js code as newer version
+
+//alert(3+3) //we are using nodejs, not browser
+
+console.log(3+3); 
+console.log()

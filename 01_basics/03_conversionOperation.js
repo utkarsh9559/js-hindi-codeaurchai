52e9a0196ab7d51933b9ff1a49e739401b3a837d
utkarsh9559 <singhutkarsh9559@gmail.com>
2025-04-27 09:23:50 +0000

    comparision

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -20,5 +20,45 @@ let isLoggedIn = 0
   let stringNumber = String(someNUmber)
   console.log(stringNumber);
   console.log(typeof stringNumber);
+
+  let value = 3
+  let negvalue = -value
+  //console.log(negvalue);
+
+ // console.log(2+2);
+  //console.log(2-2);
+ // console.log(2**2);
+  //console.log(2**3);
+ // console.log(2/3 );
+
+ let str1 = "hello"
+ let str2 = "hitesh"
+  
+  let str3 = str1+str2
+//   console.log(str3);
+//   console.log("1"+2);
+//   console.log("1"+"2");
+//   console.log("1"+2+2);
+  
+//   console.log(1+2+"2");
+  console.log(+true);
+  console.log(+"");
+
+//   let num1, num2, num3
+//   num1 = num2 = num3 = 2+2
+  let gameCounter = 100;
+  gameCounter++;
+  console.log(gameCounter);
+  
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
   
   

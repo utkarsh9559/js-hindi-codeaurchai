38b0fa70977e6338f72e874ef9d32db13ddc8e9a
utkarsh9559 <singhutkarsh9559@gmail.com>
2025-05-01 12:48:30 +0000

    jsexecution

---
+++ 03_basics/05_jsexecutioncontext.js
@@ -0,0 +1,21 @@
+     // java execution context 
+
+//{} -> 1)global execution context 
+// 2)function execution context 
+// 3)eval execultion context 
+ 
+  
+// { }->memory creation phase
+ //     2) execution phase
+ // step of exection :-
+   // 1) global exection , allocatedd in this 
+   // 2) memory phase ,saare  valriables ke naam liye ejaate hain or unke andar firstly undefined store hotqa hain 
+ // val1 - undefined
+ // val2 - undefined
+ //addnum - defination
+ //result1 - undefined
+ //res2 - undefined
+
+ // 3) execution phase  , new variable enviorment and execution thread is created every time we run the functionn 
+ //after execution it will be deleted 
+ //call stake->function calls to run

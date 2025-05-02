46f4a80aa386983cde7b0fc962a5b118800b9a47
utkarsh9559 <singhutkarsh9559@gmail.com>
2025-05-02 11:42:57 +0000

    iterations

---
+++ 05_iterations/two.js
@@ -0,0 +1,23 @@
+let index = 0;
+while (index <= 10) {
+    console.log("utkarsh is a handsome boy");
+    index = index + 2;
+    
+    
+}
+
+let myArray = ["flash"  , " batman" , "utkarsh" , "superman"]
+
+let i = 0;
+while(i < myArray.length){
+    console.log(`Real hero is ${myArray[i]}`);
+    i++;
+}
+
+//do while loop 
+ let score = 11
+
+do {
+    console.log(`score is ${score}`);
+    score++
+} while (score <= 10);

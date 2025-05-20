d965241ce4639d58eb7d1f4c948eaf1076e1a16a
utkarsh9559 <singhutkarsh9559@gmail.com>
2025-05-20 12:12:45 +0000

    promises

--- 09_advance_one/promises.js
+++ 09_advance_one/promises.js
@@ -59,3 +59,41 @@ promiseFour.then((user) => {
     
 }).finally(()=> console.log("the promise is either resolve or rejected")
 )
+
+const promisefive = new Promise(function(resolve , reject){
+    setTimeout(function(){
+        let error = true 
+        if(!error){
+            resolve({username: "javascript" , password: "123"})
+        }
+        else{
+            reject('ERROR: js went wrong')
+        }
+
+    } , 1000)
+})
+
+async function consumePromiseFive(){
+    try{
+    const response = await promisefive
+    console.log(response);
+    } 
+    catch (error){
+        console.log(error);
+        
+    }
+}
+
+consumePromiseFive()
+
+// async function getAllusers(){
+//     try {
+//         const response = await fetch('https://jsonplaceholder.typicode.com/users')
+        
+// const data = await response.json()
+// console.log(data);
+//     } catch (error) {
+//         console.log("E:" , error);
+        
+//     }
+// } 
